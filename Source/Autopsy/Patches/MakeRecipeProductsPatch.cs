1d523b095da5b54186768503a1e4bb3351d1d0f9
DenJur <denis.jurockins02@gmail.com>
2018-05-25 15:32:23 +0100

    Remove basic autopsy from butchering table. Fixes Doctors doing cooking jobs.

--- Source/Autopsy/Patches/MakeRecipeProductsPatch.cs
+++ Source/Autopsy/Patches/MakeRecipeProductsPatch.cs
@@ -14,69 +14,72 @@ namespace Autopsy
         public static void Postfix(ref IEnumerable<Thing> __result, RecipeDef recipeDef, Pawn worker,
             List<Thing> ingredients)
         {
-            RecipeInfo recipeSettings = null;
-            float skillChance = worker.GetStatValue(StatDefOf.MedicalSurgerySuccessChance);
-            if (recipeDef.Equals(AutopsyRecipeDefs.AutopsyBasic))
+            if (Constants.HumanRecipeDefs.Contains(recipeDef))
             {
-                recipeSettings = new RecipeInfo(
-                    Mod.BasicAutopsyOrganMaxChance.Value,
-                    Mod.BasicAutopsyCorpseAge.Value * 2500,
-                    Mod.BasicAutopsyBionicMaxChance.Value,
-                    Mod.BasicAutopsyMaxNumberOfOrgans.Value,
-                    Mod.BasicAutopsyFrozenDecay.Value);
-                skillChance *= Mod.BasicAutopsyMedicalSkillScaling.Value;
-            }
-            else if (recipeDef.Equals(AutopsyRecipeDefs.AutopsyAdvanced))
-            {
-                recipeSettings = new RecipeInfo(
-                    Mod.AdvancedAutopsyOrganMaxChance.Value,
-                    Mod.AdvancedAutopsyCorpseAge.Value * 2500,
-                    Mod.AdvancedAutopsyBionicMaxChance.Value,
-                    Mod.AdvancedAutopsyMaxNumberOfOrgans.Value,
-                    Mod.AdvancedAutopsyFrozenDecay.Value);
-                skillChance *= Mod.AdvancedAutopsyMedicalSkillScaling.Value;
-            }
-            else if (recipeDef.Equals(AutopsyRecipeDefs.AutopsyGlitterworld))
-            {
-                recipeSettings = new RecipeInfo(
-                    Mod.GlitterAutopsyOrganMaxChance.Value,
-                    Mod.GlitterAutopsyCorpseAge.Value * 2500,
-                    Mod.GlitterAutopsyBionicMaxChance.Value,
-                    Mod.GlitterAutopsyMaxNumberOfOrgans.Value,
-                    Mod.GlitterAutopsyFrozenDecay.Value);
-                skillChance *= Mod.GlitterAutopsyMedicalSkillScaling.Value;
-            }
-            else if (recipeDef.Equals(AutopsyRecipeDefs.AutopsyAnimal))
-            {
-                recipeSettings = new RecipeInfo(
-                    0f,
-                    0,
-                    Mod.AnimalAutopsyBionicMaxChance.Value,
-                    Mod.AnimalAutopsyMaxNumberOfOrgans.Value,
-                    0);
-                skillChance *= Mod.AnimalAutopsyMedicalSkillScaling.Value;
-            }
-
-            if (recipeSettings == null) return;
-            List<Thing> result = __result as List<Thing> ?? __result.ToList();
-            foreach (Corpse corpse in ingredients.OfType<Corpse>())
-                result.AddRange(
-                    NewMedicaRecipesUtility.TraverseBody(recipeSettings, corpse, skillChance));
-
-            if (recipeDef.Equals(AutopsyRecipeDefs.AutopsyBasic))
-            {
-                worker.needs.mood.thoughts.memories.TryGainMemory(AutopsyRecipeDefs.HarvestedHumanlikeCorpse, null);
-                foreach (Pawn pawn in worker.Map.mapPawns.SpawnedPawnsInFaction(worker.Faction))
+                RecipeInfo recipeSettings = null;
+                float skillChance = worker.GetStatValue(StatDefOf.MedicalSurgerySuccessChance);
+                if (recipeDef.Equals(AutopsyRecipeDefs.AutopsyBasic))
                 {
-                    if (pawn != worker)
+                    recipeSettings = new RecipeInfo(
+                        Mod.BasicAutopsyOrganMaxChance.Value,
+                        Mod.BasicAutopsyCorpseAge.Value * 2500,
+                        Mod.BasicAutopsyBionicMaxChance.Value,
+                        Mod.BasicAutopsyMaxNumberOfOrgans.Value,
+                        Mod.BasicAutopsyFrozenDecay.Value);
+                    skillChance *= Mod.BasicAutopsyMedicalSkillScaling.Value;
+                }
+                else if (recipeDef.Equals(AutopsyRecipeDefs.AutopsyAdvanced))
+                {
+                    recipeSettings = new RecipeInfo(
+                        Mod.AdvancedAutopsyOrganMaxChance.Value,
+                        Mod.AdvancedAutopsyCorpseAge.Value * 2500,
+                        Mod.AdvancedAutopsyBionicMaxChance.Value,
+                        Mod.AdvancedAutopsyMaxNumberOfOrgans.Value,
+                        Mod.AdvancedAutopsyFrozenDecay.Value);
+                    skillChance *= Mod.AdvancedAutopsyMedicalSkillScaling.Value;
+                }
+                else if (recipeDef.Equals(AutopsyRecipeDefs.AutopsyGlitterworld))
+                {
+                    recipeSettings = new RecipeInfo(
+                        Mod.GlitterAutopsyOrganMaxChance.Value,
+                        Mod.GlitterAutopsyCorpseAge.Value * 2500,
+                        Mod.GlitterAutopsyBionicMaxChance.Value,
+                        Mod.GlitterAutopsyMaxNumberOfOrgans.Value,
+                        Mod.GlitterAutopsyFrozenDecay.Value);
+                    skillChance *= Mod.GlitterAutopsyMedicalSkillScaling.Value;
+                }
+                else if (recipeDef.Equals(AutopsyRecipeDefs.AutopsyAnimal))
+                {
+                    recipeSettings = new RecipeInfo(
+                        0f,
+                        0,
+                        Mod.AnimalAutopsyBionicMaxChance.Value,
+                        Mod.AnimalAutopsyMaxNumberOfOrgans.Value,
+                        0);
+                    skillChance *= Mod.AnimalAutopsyMedicalSkillScaling.Value;
+                }
+
+                if (recipeSettings == null) return;
+                List<Thing> result = __result as List<Thing> ?? __result.ToList();
+                foreach (Corpse corpse in ingredients.OfType<Corpse>())
+                    result.AddRange(
+                        NewMedicaRecipesUtility.TraverseBody(recipeSettings, corpse, skillChance));
+
+                if (recipeDef.Equals(AutopsyRecipeDefs.AutopsyBasic))
+                {
+                    worker.needs.mood.thoughts.memories.TryGainMemory(AutopsyRecipeDefs.HarvestedHumanlikeCorpse, null);
+                    foreach (Pawn pawn in worker.Map.mapPawns.SpawnedPawnsInFaction(worker.Faction))
                     {
-                        pawn.needs?.mood?.thoughts?.memories.TryGainMemory(
-                            AutopsyRecipeDefs.KnowHarvestedHumanlikeCorpse, null);
+                        if (pawn != worker)
+                        {
+                            pawn.needs?.mood?.thoughts?.memories.TryGainMemory(
+                                AutopsyRecipeDefs.KnowHarvestedHumanlikeCorpse, null);
+                        }
                     }
                 }
-            }
 
-            __result = result;
+                __result = result;
+            }
         }
     }
 }
